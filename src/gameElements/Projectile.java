3a3e63d12ec028bfb32538be13f250756a64b2bb
benpendley <benpendley@bens-mbp-2.elon.edu>
2022-10-31 09:44:29 -0400

    format

--- src/gameElements/Projectile.java
+++ src/gameElements/Projectile.java
@@ -1,26 +1,26 @@
 package gameElements;
+
 import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.shape.Circle;
 
-public abstract class Projectile extends GameElement{
+public abstract class Projectile extends GameElement {
 	private Circle circle;
-	 private Point2D myVelocity = new Point2D(100, 100);
-	
-	
-	 public Bounds getBounds() {
-	    	return this.circle.getBoundsInParent();
-	    }
-	    
-	    public Node getNode() {
-	    	return this.circle;
-	    }
-	   
-	public void move (double elapsedTime) {
-        circle.setCenterX(circle.getCenterX() + myVelocity.getX() * elapsedTime);
-        circle.setCenterY(circle.getCenterY() + myVelocity.getY() * elapsedTime);
-    }
+	private Point2D myVelocity = new Point2D(100, 100);
+
+	public Bounds getBounds() {
+		return this.circle.getBoundsInParent();
+	}
+
+	public Node getNode() {
+		return this.circle;
+	}
+
+	public void move(double elapsedTime) {
+		circle.setCenterX(circle.getCenterX() + myVelocity.getX() * elapsedTime);
+		circle.setCenterY(circle.getCenterY() + myVelocity.getY() * elapsedTime);
+	}
 
 	public abstract void bounce(GameElement ge);
 
