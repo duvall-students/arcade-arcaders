e14bf7d098bd5802b103672b15690da7a0fc9a70
ethan <ethan@10.112.43.74>
2022-11-01 16:33:16 -0400

    Generalized GameState more, need to implement some of Galaga still

--- src/gamestates/GalagaGameState.java
+++ src/gamestates/GalagaGameState.java
@@ -5,44 +5,46 @@ import gameElements.*;
 //Created by Ethan Jeffries
 //This class is a child class of GameState and will handle anything that is specific to the Galaga game
 
-public class GalagaGameState extends GameState{
+public abstract class GalagaGameState extends GameState{
 	
 	protected final int numOfAlienRows;
-	private final int AlienOffset = 10;
+	private final int TARGETOFFSET = 10;
+	//POSSIBLY CHANGE HOW THESE ARE FOUND
+	private final int TARGETHEIGHT = 25;
+	private final int TARGETWIDTH = 25;
 
+	
 	public GalagaGameState(int screenWidth, int screenHeight, int numOfAlienRows) {
 		super(screenWidth, screenHeight);
 		this.numOfAlienRows = numOfAlienRows;
-		this.spawnGameTargets();
-	}
-
-	//The rest of the class is fairly empty and will be soon implemented once we have arcade structure properly set up
-	//Method that spawns in game items which will override the parent method
-	@Override
-	public void spawnGameTargets() {
-		for (int j = 0; j < numOfAlienRows; j ++) {
-			for (int i = 0; i < screenWidth / (Alien.alienWidth + AlienOffset); i ++) {
-				Alien alienCreated = new Alien(AlienOffset * i + Alien.alienWidth * i + AlienOffset, AlienOffset + j * Alien.alienHeight, this);
-				gameTargets.add(alienCreated);
-				root.getChildren().add(alienCreated.getNode());
-			}
-		}
-
-	}
-	
-	@Override
-	public void spawnGameProjectiles() {
-		
-	}
-	
-	@Override
-	public void spawnPlayerMover() {
-		
+		super.spawnGameTargets(numOfAlienRows, TARGETOFFSET, TARGETWIDTH, TARGETHEIGHT);
 	}
 	
 	//Make step method that is called every frame
 	public void makeGameStep(double elapsedTime) {
 		
 	}
+
+	//The rest of the class is fairly empty and will be soon implemented once we have arcade structure properly set up
+	//Method that spawns in game items which will override the parent method
+	@Override
+	public Target createTarget(int targetXCoordinate, int targetYCoordinate) {
+		return new Alien(targetXCoordinate, targetYCoordinate, this);
+	}
+	
+	@Override
+	public Projectile createProjectile() {
+		//Bullet constructor does not exist yet but soon will
+//		return new Bullet(screenWidth, screenHeight);
+
+	}
+	
+	@Override
+	public PlayerMover createPlayerMover() {
+		//Spaceship constructor does not exist yet but soon will
+//		return new Spaceship(screenWidth, screenHeight);
+	}
+	
+
 
 }
